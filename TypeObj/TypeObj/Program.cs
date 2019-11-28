e81a22d59467c56e2fd2136b7b9a859b77c6ec70
Student <Student@201-10>
2019-11-28 21:04:17 +0200

    Data Type

---
+++ TypeObj/TypeObj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeObj
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Object obj;
+			bool bvar = false;
+			//or
+			System.Boolean bvar2 = true;
+
+			sbyte b;
+			
+		}
+	}
+}
