b7b903cdb39c9a42dad0d020cfaaac3715096298
Student <Student@201-10>
2019-12-10 19:25:05 +0200

    d

---
+++ BigNum/Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter the Array [size]: ");
+			/*int size = Convert.ToInt32(Console.ReadLine());
+			int[] arr = new int[size];*/
+			int[] arr = { 3, 5, 8, 13, 21 };
+
+			
+			//Random rand = new Random();
+			/*for(int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = rand.Next(100, 200);
+			}
+			/*for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i] + "\t");
+			}*/
+			foreach (int i in arr)
+			{
+				Console.WriteLine(i + '\t');
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ BigNum/BigNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace BigNum
+{
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			Console.Write("Enter the value: ");
+			try
+			{
+				
+				BigInteger n = Convert.ToUInt32(Console.ReadLine());
+			
+
+			
+			BigInteger f = 1;
+			for(ulong i = 1; i <= n; i++)
+			{
+				f *= i;
+				//Console.WriteLine($"{i}!={f}");
+			}
+			Console.WriteLine($"{n}!={f}");
+			}
+			catch
+			{
+				Console.WriteLine("Введите числовое значение в десятичной системе исчисления!");
+			}
+		}
+	}
+}
