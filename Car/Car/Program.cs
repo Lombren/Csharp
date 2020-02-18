cbe3b84cce9b311ea03d28c34a4f797010963636
Student <Student@304-1>
2020-02-18 21:26:45 +0200

    Fuel_Tank_done

---
+++ Car/Car/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Tank tank = new Tank(40);
+			Console.WriteLine(tank);
+		}
+	}
+}
---
+++ Car/Car/Tank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+	class Tank
+	{
+		uint volume;
+		double fuel;
+		public uint Volum
+		{
+			get=>volume;
+			private set => volume = value >= 40 && value <= 120 ? value : throw new Exception("Error: Bad tank volum");
+		}
+		public double Fuel
+		{
+			get => fuel;
+			set
+			{
+				fuel += value;
+				if (fuel < 0) fuel = 0;
+				if (fuel > Volum) fuel = Volum;
+			}
+		}
+		public Tank(uint volum)
+		{
+			Volum = volum;
+			Fuel = fuel;
+
+		}
+		void Fill(double fuel)
+		{
+			Fuel += fuel;
+		}
+		public override string ToString()
+		{
+			return $"Tank volume: {Volum};\nFuel level: {Fuel};";
+		}
+
+	}
+}
